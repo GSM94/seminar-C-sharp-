8917c82cf787bf418fb68b1cd2f09490fd94028d
Shamil <shoma994@gmail.com>
2022-12-03 17:11:47 +0300

    lesson_1

---
+++ lesson_1/Program.cs
@@ -0,0 +1,45 @@
+﻿// int a = 5, b = 3;
+// // double b = 2.5;
+// // bool c = a > b;
+
+// // string d = "word ) % #";
+// // char e = '%'; // для одного символа, в одних ковычках
+
+
+// // Console.WriteLine("My number are " + a + " and " + b + " and it is good!"); // таким образом получается длинная строка
+// Console.WriteLine($"My number are {a} and  {b} and it is good!"); //с помощью $ строка становится короче. Переменнные записываются в фигурных скобках {a}
+
+// Является ли число квадратом второго?
+// Console.Write("Input a first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int quad = num2 * num2;
+// if (num1 == quad)
+// {
+//     Console.WriteLine("Yes!"); // можно без фигурных скобок, если только одно действие 
+// }
+// else
+// {
+//     Console.WriteLine("No!");
+// }
+
+//Задача 2. Напишите программу, которая на вход принимает целое число (N), а на выходе показывает покажет вце целые числа от -n до n.
+
+// Console.Write("Input an integer number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int current = -num;
+
+// while(current <= num)
+// {
+//     Console.Write(current + " ");
+//     current++;
+// }
+
+12345 / 10 = 1234
+12345 / 100 = 123
+12345 / 1000 = 12
+12345 / 10000 = 1
+
